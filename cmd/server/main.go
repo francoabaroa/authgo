dd81eefa22962c1f370245774ae42f06ff5f4356
Franco Abaroa <franco9395@gmail.com>
2023-07-14 14:25:04 -0400

    Register routes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+	"github.com/francoabaroa/authgo/pkg/handlers"
+)
+
+type HelloResponse struct {
+	Hello string `json:"hello"`
+}
+
+type SuccessResponse struct {
+	Success string `json:"success"`
+}
 
 func main() {
-    fmt.Println("Hello, world!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		response := SuccessResponse{
+			Success: "ful",
+		}
+
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonResponse)
+	})
+
+	http.HandleFunc("/register", handlers.RegisterHandler)
+	http.HandleFunc("/login", handlers.LoginHandler)
+	http.HandleFunc("/reset_password", handlers.ResetPasswordHandler)
+
+	http.ListenAndServe(":8080", nil)
 }
